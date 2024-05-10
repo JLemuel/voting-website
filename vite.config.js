73769688fac0d6bade0701a0aa063cec1fa08f8b
JLemuel <79501195+JLemuel@users.noreply.github.com>
2024-05-10 14:38:32 +0800

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    build: {
-        outDir: "public/dist", // Output directory for compiled assets
-        manifest: true, // Generate manifest file
-        rollupOptions: {
-            // Other rollup options...
-        },
-    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
