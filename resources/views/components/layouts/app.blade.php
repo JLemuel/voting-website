10c906f7ccb0c751423678c8e20468cc37a981e9
JLemuel <79501195+JLemuel@users.noreply.github.com>
2024-05-10 14:39:37 +0800

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,7 +9,6 @@
     {{--
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> --}}
     <title>{{ isset($title) ? $title.' - '.config('app.name') : config('app.name') }}</title>
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
